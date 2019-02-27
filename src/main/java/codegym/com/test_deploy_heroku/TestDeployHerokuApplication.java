c4e79d29637c671484c0e22d0cde1c5258cfc998
TranVietDuc <vietduc3k@gmail.com>
2019-02-27 11:40:35 +0700

    First commit

---
+++ src/main/java/codegym/com/test_deploy_heroku/TestDeployHerokuApplication.java
@@ -0,0 +1,13 @@
+package codegym.com.test_deploy_heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDeployHerokuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codegym/com/test_deploy_heroku/TestDeployHerokuApplicationTests.java
@@ -0,0 +1,16 @@
+package codegym.com.test_deploy_heroku;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestDeployHerokuApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
